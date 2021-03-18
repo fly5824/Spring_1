44a6dde551b55670415472fbff9faf8a913b189c
uooh_Z <fly5148@naver.com>
2021-03-18 14:47:29 +0900

    c

--- src/main/java/com/naver/s1/member/MemberService.java
+++ src/main/java/com/naver/s1/member/MemberService.java
@@ -1,7 +1,12 @@
 package com.naver.s1.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
 
+	@Autowired
 	private MemberDAO memberDAO;
 
 	public MemberDTO memberlogin(MemberDTO memberDTO) throws Exception {
